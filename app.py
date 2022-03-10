bed43a02b09b7ca51f92d30c5397f8e63d6c50d7
Saqlain Alam Artas <saqlainartaz@gmail.com>
2022-03-10 10:00:00 +0100

    chore: scaffold Flask application structure
    Initialize project requirements with Flask, Flask-Session, and cs50
    for SQLite access. Set up the base Flask app with session configuration,
    database connection, and a login_required decorator for protected routes.

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, flash, redirect, render_template, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_session import Session
+from functools import wraps
+from cs50 import SQL
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+db = SQL("sqlite:///mydatabase.db")
+
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
